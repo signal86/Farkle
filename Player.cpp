5a1e3176c75690170c69bda7d24a27baf592d68e
signal86 <diafakemail@gmail.com>
2024-05-08 13:04:13 -0500

    test

--- Player.cpp
+++ Player.cpp
@@ -18,6 +18,7 @@ class Player {
     Dice dice[6] = {Dice(), Dice(), Dice(), Dice(), Dice(), Dice()};
     int turnScore = 0;
     string username;
+    bool ignore;
 
     Player() {}
     Player(string name) : username(name) { }
